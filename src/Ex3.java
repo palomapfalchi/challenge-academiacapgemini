f3ddb45039520657a20cac7846620d1487ddc574
palomapfalchi <85750919+palomapfalchi@users.noreply.github.com>
2022-02-19 15:19:13 -0300

    refatorando o codigo

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,35 +1,29 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ex3 {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a palavra que você quer analisar: ");
         String palavra = scanner.nextLine();
 
-        ArrayList<String> todasStrings = new ArrayList<>();
         ArrayList<String> anagramas = new ArrayList<>();
 
-        int countIguais = 0;
-
-        for (int i = 0; i < palavra.length() - 1; i++) {
+        for (int i = 0; i < palavra.length(); i++) {
             for (int j = 1 + i; j < palavra.length(); j++) {
-
-                char charI = palavra.charAt(i);
-                char charJ = palavra.charAt(j);
-
-                String novaString = palavra.substring(i, j + 1);
-
-                if (i != j) {
-                    if (charI == charJ) {
-                        countIguais = countIguais + 1;
-                        anagramas.add(palavra.substring(i, i+1));
+                if (palavra.charAt(i) == palavra.charAt(j) && i != j && i < j) {
+                    if (palavra.charAt(i) == palavra.charAt(i+1)) {
+                        String substring = palavra.substring(i, i+1);
+                        anagramas.add(substring.repeat(2));
+                    } else {
+                        anagramas.add(palavra.substring(i,i+1).repeat(2));
+                        anagramas.add(palavra.substring(i+1, j+1));
+                        anagramas.add(palavra.substring(i,j));
                     }
-                    todasStrings.add(novaString);
                 }
             }
         }
-        System.out.println("Anagramas: " + anagramas.size() + " - " + anagramas);
-        System.out.println("Todas Substrings: " + todasStrings);
+        System.out.println("Anagramas: " + anagramas);
+        System.out.println(anagramas.size());
     }
 }
